26cc621fbebbffd59f7090d2baeb69963c7d06c1
idlerelic <relicidle@gmail.com>
2023-10-08 01:04:07 +0100

    Added 404

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>404 - Page Not Found</h1>
+      <p>The page you requested could not be found.</p>
+      <p>Return to <a href="/">Home</a></p>
+    </div>
+  );
+};
+
+export default NotFound;
